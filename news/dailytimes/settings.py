d156f1a1af498206fac942e6c7169f8b7f44c9fd
abena1apaw <57848351+abena1apaw@users.noreply.github.com>
2020-06-27 20:52:43 -0500

    created debug toolbar

--- news/dailytimes/settings.py
+++ news/dailytimes/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'crispy_forms',
+    'debug_toolbar',
 
     'users.apps.UsersConfig',
     'pages.apps.PagesConfig',
@@ -52,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'dailytimes.urls'
@@ -144,5 +146,8 @@ EMAIL_USE_TLS = True
 
 TIME_ZONE = 'America/Chicago'
 
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+INTERNAL_IPS = ('127.0.0.1', )
